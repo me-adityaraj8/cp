456e968fd0c919e674985cd530a6a7f7f13ce33a
me-adityaraj8 <me.adityaraj8@gmail.com>
2026-08-22 22:06:06 +0530

    Add Alternating Series [B] from Codeforces

---
+++ 2131/B - Alternating Series/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) cout << -1;
+            else cout << (i == n ? 2 : 3);
+            if (i < n) cout << " ";
+        }
+        cout << "
+";
+    }
+    return 0;
+}
