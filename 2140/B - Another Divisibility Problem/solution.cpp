87c98deb9c0fc3623e4638246d3c8403b157000a
me-adityaraj8 <me.adityaraj8@gmail.com>
2026-08-22 22:07:56 +0530

    Add Another Divisibility Problem [B] from Codeforces

---
+++ 2140/B - Another Divisibility Problem/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long x; cin>>x;
+        cout << 8*x << '
+';
+    }
+    return 0;
+}
+ 
