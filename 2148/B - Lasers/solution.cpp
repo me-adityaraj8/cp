3915027e6f83f6f512b5f7a2afa147f69cc1811d
me-adityaraj8 <me.adityaraj8@gmail.com>
2026-08-22 22:08:19 +0530

    Add Lasers [B] from Codeforces

---
+++ 2148/B - Lasers/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    if(!(cin >> t)) return 0;
+    while(t--) {
+        long long n,m,x,y;
+        cin >> n >> m >> x >> y;
+        for(long long i=0, v;i<n;i++) cin >> v;
+        for(long long j=0, v;j<m;j++) cin >> v;
+        cout << (n + m) << '
+';
+    }
+    return 0;
+}
